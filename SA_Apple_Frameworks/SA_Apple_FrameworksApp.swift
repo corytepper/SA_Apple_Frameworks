c96bc42fad4d9b6f573c1d33bf211e5986128b17
Cory Tepper <corytepper@gmail.com>
2022-12-11 17:30:27 -0500

    Initial Commit

---
+++ SA_Apple_Frameworks/SA_Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SA_Apple_FrameworksApp.swift
+//  SA_Apple_Frameworks
+//
+//  Created by Cory Tepper on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SA_Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
