afeb4227d94f5ffc6739fa76ac9b5ef60c56e54d
Cory Tepper <corytepper@gmail.com>
2022-12-13 14:38:03 -0500

    FrameworkDetailView

---
+++ SA_Apple_Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,52 @@
+//
+//  FrameworkDetailView.swift
+//  SA_Apple_Frameworks
+//
+//  Created by Cory Tepper on 12/13/22.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }.padding(10)
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+               AFButton(title: "Learn More")
+                    
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+    }
+}
